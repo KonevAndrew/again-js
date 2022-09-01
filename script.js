82edecf087a8318503bd23165bf2c18b51e13fc0
KonevAndrew <a9139286138@yandex.ru>
2022-09-01 22:20:29 +0700

    логические операторы

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
@@ -57,3 +57,44 @@ if ( andrew && mark) {
     console.log('yes yes');
 }
 
+
+                          //оператор &&(и) выводит false если одно из условий не выполнится
+                          //оператор || (или) выводит true если одно из условий true
+
+console.log(NaN || 2 || undefined);                   //2 так как это первое true
+
+console.log(NaN && 2 && undefined);                   //nan так как первое false
+
+console.log(1 && 2 && 3);                             //веыдедт 3 т.к. последнее true
+
+console.log((!1 && 2) || !3);                         //false
+
+console.log(25 || (null && !3));                       //25
+
+console.log(NaN || null || !3 || undefined || 5);     //5
+
+console.log(NaN || (null && !3 && undefined) || 5);   //5
+
+console.log((5 === 5 && 3 > 1) || 5);                 //выводит первое true, а это 5===5
+
+
+const hamburger = 3;                                       //условие выполнено т.к. справа true
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+  console.log("Done!");
+}
+
+
+let hamburgerr;
+const friess = NaN;
+const colaa = 0;
+const nuggetss = 0;
+
+if (hamburgerr || colaa || friess === 3 || nuggetss) {
+  console.log("Done!");
+} else {
+  console.log('ne done')
+};
